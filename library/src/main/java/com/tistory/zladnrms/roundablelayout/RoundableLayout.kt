e9cc8379709c14b04196f2443b6340a662100d48
zladnrms <kwk@onns.co.kr>
2019-10-02 14:58:11 +0900

    init

---
+++ library/src/main/java/com/tistory/zladnrms/roundablelayout/RoundableLayout.kt
@@ -0,0 +1,60 @@
+package com.tistory.zladnrms.roundablelayout
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.util.TypedValue
+
+class RoundableLayout : ConstraintLayout {
+
+    private var path: Path? = null
+    private var cornerLeftTop: Float = 0F
+    private var cornerRightTop: Float = 0F
+    private var cornerLeftBottom: Float = 0F
+    private var cornerRightBottom: Float = 0F
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setCornerRound(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setCornerRound(context, attrs)
+    }
+
+    constructor(context: Context) : super(context) {
+        setCornerRound(context, null)
+    }
+
+    private fun setCornerRound(context: Context, attrs: AttributeSet?) {
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RoundableLayout)
+            cornerLeftTop = typedArray.getDimensionPixelSize(R.styleable.RoundableLayout_cornerRightBottom,0).toFloat()
+            cornerRightTop = typedArray.getDimensionPixelSize(R.styleable.RoundableLayout_cornerRightTop,0).toFloat()
+            cornerLeftBottom = typedArray.getDimensionPixelSize(R.styleable.RoundableLayout_cornerLeftBottom,0).toFloat()
+            cornerRightBottom = typedArray.getDimensionPixelSize(R.styleable.RoundableLayout_cornerLeftTop,0).toFloat()
+
+            typedArray.recycle()
+        }
+    }
+
+    private fun dpToFloat(context: Context, value: Float): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, context.resources.displayMetrics)
+    }
+
+    override fun dispatchDraw(canvas: Canvas) {
+        if(path == null) {
+            path = Path()
+        }
+
+        val radii = floatArrayOf(cornerLeftTop, cornerLeftTop, cornerRightTop, cornerRightTop, cornerRightBottom, cornerRightBottom, cornerLeftBottom, cornerLeftBottom)
+
+        path?.let {
+            it.addRoundRect(RectF(0F,0F,canvas.width.toFloat(), canvas.height.toFloat()), radii, Path.Direction.CW)
+            canvas.clipPath(it)
+        }
+        super.dispatchDraw(canvas)
+    }
+}
